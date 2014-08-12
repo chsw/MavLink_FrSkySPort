7741c1995363ae7d80efce444166c324fc2f3a56
ddyeakley <ddyeakley@gmail.com>
2014-08-12 16:09:25 -0400

    Updated to defines to align with FrSkySPort.h
    Reordered defines.  Add a few missing defines that might be useful.
    Changed SENSOR_ID_FLVSS to A1.

--- MavLink_FrSkySPort/FrSkySPort.h
+++ MavLink_FrSkySPort/FrSkySPort.h
@@ -1,34 +1,43 @@
 // Frsky Sensor-ID to use. 
-#define SENSOR_ID_FLVSS              0x1B
 #define SENSOR_ID_VARIO              0x00
+#define SENSOR_ID_FLVSS              0xA1  
 #define SENSOR_ID_FAS                0x22
 #define SENSOR_ID_GPS                0x83
 #define SENSOR_ID_RPM                0xE4
-// Frsky-specific
-#define START_STOP               0x7e
-#define DATA_FRAME               0x10
+#define SENSOR_ID_SP2UH              0x45 
+#define SENSOR_ID_SP2UR              0xC6 
 
+// Frsky-specific
+#define START_STOP                   0x7e
+#define DATA_FRAME                   0x10
 
 //Frsky DATA ID's 
-#define FR_ID_SPEED               0x0830 
-#define FR_ID_VFAS                 0x0210 
-#define FR_ID_CURRENT         0x0200 
-#define FR_ID_RPM                  0x050F      
-#define FR_ID_ALTITUDE         0x0100
-#define FR_ID_FUEL                 0x0600   
-#define FR_ID_ADC1                0xF102   
-#define FR_ID_ADC2                0xF103      
-#define FR_ID_LATLONG         0x0800
-#define FR_ID_CAP_USED       0x0600
-#define FR_ID_VARIO               0x0110
-#define FR_ID_CELLS              0x0300     
-#define FR_ID_CELLS_LAST   0x030F      
-#define FR_ID_HEADING          0x0840
-#define FR_ID_ACCX               0x0700
-#define FR_ID_ACCY               0x0710
-#define FR_ID_ACCZ               0x0720
+#define FR_ID_ALTITUDE               0x0100
+#define FR_ID_VARIO                  0x0110
+#define FR_ID_VFAS                   0x0210 
+#define FR_ID_CURRENT                0x0200 
+#define FR_ID_CELLS                  0x0300     
+#define FR_ID_CELLS_LAST             0x030F      
 #define FR_ID_T1                     0x0400
 #define FR_ID_T2                     0x0410
-#define FR_ID_GPS_ALT          0x0820
+#define FR_ID_RPM                    0x050F      
+#define FR_ID_FUEL                   0x0600   
+#define FR_ID_CAP_USED               0x0600
+#define FR_ID_ACCX                   0x0700
+#define FR_ID_ACCY                   0x0710
+#define FR_ID_ACCZ                   0x0720
+#define FR_ID_LATLONG                0x0800
+#define FR_ID_GPS_ALT                0x0820
+#define FR_ID_SPEED                  0x0830 
+#define FR_ID_HEADING                0x0840
+#define FR_ID_GPS_TIME_DATE_FIRST    0x0850
+#define FR_ID_A3_FIRST               0x0900
+#define FR_ID_A4_FIRST               0x0910
+#define FR_ID_AIR_SPEED_FIRST        0x0a00
+#define FR_ID_RSSI                   0xf101
+#define FR_ID_ADC1                   0xF102   
+#define FR_ID_ADC2                   0xF103      
+#define FR_ID_BATT                   0xF104
+#define FR_ID_SWR                    0xF105
 
 
