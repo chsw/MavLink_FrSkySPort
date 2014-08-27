37a44ae7b96b2365c9d646b99dfda61c0bfe6f9d
Christian <christian.swahn@gmail.com>
2014-08-27 20:13:02 +0200

    Added timestamp (time since message) to log-screen.

--- Lua_Telemetry/Arducopter_Status/SCRIPTS/_modelname_/telem3.lua
+++ Lua_Telemetry/Arducopter_Status/SCRIPTS/_modelname_/telem3.lua
@@ -5,6 +5,23 @@ end
 local messages = {}
 local last_message = ""
 
+
+local function asTimeNumber(number)
+	if number < 10
+	then
+		return "0"..number
+	end
+	return number
+end
+
+local function formatTimestamp(timestamp)
+  local iDiff, fDiff = math.modf((getTime() - timestamp)/100)
+  local sec = iDiff%60
+  local min = (iDiff-sec)/60
+  local ret = asTimeNumber(min)..":"..asTimeNumber(sec)
+  return ret
+end
+
 local function handleMessage()
   -- Fetch message
   local new_message = getApmActiveWarnings(true)
@@ -25,7 +42,7 @@ local function handleMessage()
 	do
 	  messages[i] = messages[i-1]
 	end
-	messages[1] = new_message
+	messages[1] = {text = new_message, timestamp = getTime() }
   end
 end
 
@@ -44,7 +61,7 @@ local function run(event)
   do
 	if messages[i] ~= nil
 	then
-		lcd.drawText(1, 1 + 8* (i-1) , messages[i], 0)
+		lcd.drawText(1, 1 + 8* (i-1) , formatTimestamp(messages[i].timestamp).."  "..messages[i].text, 0)
 		warnings_received = true
 	end
   end
