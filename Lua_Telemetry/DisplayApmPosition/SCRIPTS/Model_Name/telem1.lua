8f3edacef8f875177e25d6ea3be8d92560a214f1
lvale <Luis@Sparkle.lan>
2014-10-21 22:30:51 +0100

    Added GNU

--- Lua_Telemetry/DisplayApmPosition/SCRIPTS/Model_Name/telem1.lua
+++ Lua_Telemetry/DisplayApmPosition/SCRIPTS/Model_Name/telem1.lua
@@ -1,3 +1,20 @@
+-- Copyright Luis Vale Gonçalves.
+-- 	  This program is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    A copy of the GNU General Public License is available at <http://www.gnu.org/licenses/>.
+--    
+
+
+
+
 --Auxiliary files on github under dir BMP and SOUNDS/en
 -- https://github.com/lvale/MavLink_FrSkySPort/tree/DisplayAPMPosition/Lua_Telemetry/DisplayApmPosition
 
