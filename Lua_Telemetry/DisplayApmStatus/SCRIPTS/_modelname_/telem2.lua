a5450cd464a3bf86cd302b411f9ce26854ab1b7d
Christian <christian.swahn@gmail.com>
2014-11-01 10:34:55 +0100

    Optimized code.

--- Lua_Telemetry/DisplayApmStatus/SCRIPTS/_modelname_/telem2.lua
+++ Lua_Telemetry/DisplayApmStatus/SCRIPTS/_modelname_/telem2.lua
@@ -1,32 +1,35 @@
 -- Don't change these
-local API_LEVEL_MAJOR = 2
-local API_LEVEL_MINOR = 1
+local apiMajor = 2
+local apiMinor = 1
 
-local messages = {}
-local last_message = nil
+local amsg = {}
+local amsgsize = 0
+local lmsg = nil
+local i
 global_new_messages = false
 
+local dt=lcd.drawText
 local ApmTelem = nil
 		
 local function init()
 	ApmTelem = getApmTelem()
 end
 
-local function checkVersionVersion()
-  if ApmTelem == nil then
-	lcd.drawText(20, 25, "Please install ApmTelem.lua", 0)
-	lcd.drawText(20, 35, "on the \"Custom Scripts\" page!", 0)
-  elseif ApmTelem.VER_MAJOR > API_LEVEL_MAJOR then
-  	lcd.drawText(10, 20, "This telemetry screen is to old for", 0)
-	lcd.drawText(10, 30, "the installed version of ApmTelem.lua", 0)
-	lcd.drawText(10, 40, "Please upgrade", 0)
-  elseif ApmTelem.VER_MAJOR < API_LEVEL_MAJOR or ApmTelem.VER_MINOR < API_LEVEL_MINOR then
-  	lcd.drawText(20, 25, "Please upgrade ApmTelem.lua", 0)
-	lcd.drawText(20, 35, "on the \"Custom Scripts\" page!", 0)
-  else
-	return 0
-  end
-  return 1
+local function chkVer()
+	if ApmTelem == nil then
+		dt(20, 25, "Please install ApmTelem.lua", 0)
+		dt(20, 35, "on the \"Custom Scripts\" page!", 0)
+	elseif ApmTelem.VER_MAJOR > apiMajor then
+		dt(10, 20, "This telemetry screen is to old for", 0)
+		dt(10, 30, "the installed version of ApmTelem.lua", 0)
+		dt(10, 40, "Please upgrade", 0)
+	elseif ApmTelem.VER_MAJOR < apiMajor or ApmTelem.VER_MINOR < apiMinor then
+		dt(20, 25, "Please upgrade ApmTelem.lua", 0)
+		dt(20, 35, "on the \"Custom Scripts\" page!", 0)
+	else
+		return 0
+	end
+	return 1
 end
 
 --function overrideApmStatusMessage(message)
@@ -40,7 +43,7 @@ end
 --end
 
 -- Format a number to a string with 2 digits. 
-local function asTimeNumber(number)
+local function toTwoDigit(number)
 	if number < 10
 	then
 		return "0"..number
@@ -49,88 +52,69 @@ local function asTimeNumber(number)
 end
 
 -- Takes a time (10 msec resolution) and formats as min:sec
-local function formatTimestamp(timestamp)
-  local iDiff, fDiff = math.modf((getTime() - timestamp)/100)
-  local sec = iDiff%60
-  local min = (iDiff-sec)/60
-  local ret = asTimeNumber(min)..":"..asTimeNumber(sec)
-  return ret
+local function fmtTime(timestamp)
+	local iDiff, fDiff = math.modf((getTime() - timestamp)/100)
+	local sec = iDiff%60
+	local min = (iDiff-sec)/60
+	local ret = toTwoDigit(min)..":"..toTwoDigit(sec)
+	return ret
 end
 
 -- Looks for new messages and builds a list of messages
-local function handleMessage()
-  -- Fetch message
-  local message = ApmTelem.getActiveStatus()
-  if message ~= nil and (last_message == nil or message.timestamp ~= last_message.timestamp)
-  then 
-	-- If message is marked as silent - don't activte global flag
-    if message.silent == false
-	then
-		global_new_messages = true
+local function scanMsg()
+	-- Fetch message
+	local m = ApmTelem.getActiveStatus()
+	if m ~= nil and (lmsg == nil or m.timestamp ~= lmsg.timestamp)
+	then 
+		-- If message is marked as silent - don't activte global flag
+		if not m.silent
+		then
+			global_new_messages = true
+		end
+		-- Store message as last message
+		lmsg = m
+		amsgsize = math.max(amsgsize+1, 7)
+		-- Move all stored messages back in array
+		for i=amsgsize, 2, -1
+		do
+			amsg[i] = amsg[i-1]
+		end
+		-- Put the last message first
+		amsg[1] = {text = m.message, timestamp = m.timestamp, severity = m.severity }
 	end
-	-- Store message as last message
-    last_message = message
-	-- Search for the next free position
-	local i = 1
-    while messages[i] ~= nil 
-	do
-      i = i + 1
-	  -- Limit history length
-	  if i >= 20
-	  then
-		break
-	  end
-    end
-	-- Move all stored messages back in array
-	for i=i, 2, -1
-	do
-	  messages[i] = messages[i-1]
-	end
-	-- Put the last message first
-	messages[1] = {text = message.message, timestamp = message.timestamp, severity = message.severity }
-
-  end
 end
 
 -- Scan for messages when not visible
 local function background()
-  if checkVersionVersion() > 0 then
-    return
-  end
-  handleMessage()
+	if chkVer() > 0 then return end
+	scanMsg()
 end
 
-
 local function run(event)
-  if checkVersionVersion() > 0 then
-    return
-  end
+	if chkVer() > 0 then return end
   
-  -- Scan for new messages
-  handleMessage()
+	-- Scan for new messages
+	scanMsg()
 
-  -- Reset global flag since we now have shown any new messages
-  global_new_messages = false
+	-- Reset global flag since we now have shown any new messages
+	global_new_messages = false
   
-  -- Loop through messages
-  local i = 1
-  local warnings_received = false
-  for i=1, 10, 1
-  do
-	if messages[i] ~= nil
-	then
-		lcd.drawPixmap(1, 1+8* (i-1), "/SCRIPTS/BMP/severity" .. messages[i].severity .. ".bmp")
-		lcd.drawText(10, 1 + 8* (i-1) , formatTimestamp(messages[i].timestamp).."  "..messages[i].text, 0)
-		warnings_received = true
+	-- Loop through messages
+	if amsgsize > 0 then
+		for i=1, 7, 1
+		do
+			if amsg[i] ~= nil
+			then
+				lcd.drawPixmap(1, 1+8* (i-1), "/SCRIPTS/BMP/severity" .. amsg[i].severity .. ".bmp")
+				dt(10, 1 + 8* (i-1) , fmtTime(amsg[i].timestamp).."  "..amsg[i].text, 0)
+				empty = false
+			end
+		end
+	-- No messages received - show text
+	else
+		lcd.drawFilledRectangle(10, 10, 190, 45, GREY_DEFAULT)
+		dt(40, 30, "No warnings received", 0)
 	end
-  end
-  -- No messages received - show text
-  if warnings_received == false
-  then
-	lcd.drawFilledRectangle(10, 10, 190, 45, GREY_DEFAULT)
-	lcd.drawText(40, 30, "No warnings received", 0)
-  end
-  
 end
 
 return { init=init, run=run, background=background}
