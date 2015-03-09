e76758a2dfda3772c539dedc66c7fcb8300ae57b
Christian <christian.swahn@gmail.com>
2015-03-09 21:16:54 +0100

    Second revision
    Added command que for channel changing (to prepare for delayed
    commands).

--- Lua_Telemetry/Storm32/SCRIPTS/_modelname_/telem5.lua
+++ Lua_Telemetry/Storm32/SCRIPTS/_modelname_/telem5.lua
@@ -2,22 +2,14 @@ local menu={}
 menu[0] = {label = "Pan Mode Control", channel=16, cases={[0]="hold hold pan", [1]="hold hold hold", [2]="pan pan pan"}}
 menu[1] = {label = "Pitch control", channel=12, cases={[0]="Apm", [1]="offline", [2]="User pitch"}}
 menu[2] = {label = "Recenter camera", channel=15, cases={[0]="Normal", [1]="Recenter"}}
-menu[3] = {label = "Script 1", channel=nil}
-menu[4] = {label = "Script 2", channel=nil}
-menu[5] = {label = "Script 3", channel=nil}
-menu[6] = {label = "Script 4", channel=11, cases={[0]="Case#Default", [1]="Case#1", [2]="Case#2", [3]="Case#3"}}
-menu[7] = {label = "Script 5", channel=16, cases={[0]="default", [1]="tmp"}}
-menu[8] = {label = "Script 6", channel=nil}
-menu[9] = {label = "Script 7", channel=nil}
 
 local menu_index = 0
 local display_rows = 6
-local function init()
-end
 
-local function background()
-
-end
+local __it = nil
+local function getIt() if __it == nil then return {next=nil, data=nil} end return {next=__it, data=nil} end
+local function addLI(data) if __it == nil then __it = {next=nil, data=data} else local __t = __it while __t.next ~=nil do __t = __t.next end __t.next = {next=nil, data=data} end end
+local function removeLI(item) if __it == nil or item==nil then return getIt() end if __it == item then __it = item.next return {next=__it, data=nil} end local __t = getIt() while __t.next ~= nil and __t.next  ~= item do __t = __t.next end if __t.next ~= nil then __t.next = __t.next.next end return __t end
 
 local function parseValue(item)
 	if item == nil or item.channel == nil then
@@ -71,8 +63,42 @@ local function ternary(value, first, second)
 	return second
 end
 
+local function updateChannel(channel, case)
+	local mixLine = model.getMix(channel-1, 0)
+	if mixLine.source ~= 84 then -- MAX 
+		return
+	end
+	if case == 0 then mixLine.weight=0
+	elseif case == 1 then mixLine.weight=100
+	elseif case == 2 then mixLine.weight=-100
+	elseif case == 3 then mixLine.weight=70
+	end
+
+	model.insertMix(channel-1, 0, mixLine)
+	model.deleteMix(channel-1, 1)
+end
+
+local function doBgWork()
+	local t = getIt()
+	while t.next ~= nil 
+	do
+		t = t.next
+		if t.data.timeout<getTime() 
+		then
+			updateChannel(t.data.channel, t.data.case)
+			t = removeLI(t)
+		end
+	end
+end
+local function init()
+end
+local function background()
+	doBgWork()
+end
 local function run(event)
+	doBgWork()
 	lcd.drawScreenTitle("STorM32 Control Panel", 1, 1)
+	-- Handle row change
 	if event == EVT_MENU_BREAK
 	then
 		menu_index = menu_index +1
@@ -81,7 +107,7 @@ local function run(event)
 			menu_index = 0
 		end
 	end
-	
+	-- Display rows
 	local start = ternary(menu_index <display_rows, 0, menu_index -display_rows+1)
 	local row = 0
 	for i=start, start+display_rows-1, 1
@@ -99,16 +125,15 @@ local function run(event)
 		lcd.drawText(100, 9+8*row, parseValueStr(menu[i]), 0);
 		row = row+1
 	end
-	
+	-- check selected row
 	local item = menu[menu_index]
 	if item.channel ~= nil then
 		local showSubmenu = false
-		if model.getMixesCount(item.channel-1) == 1 then
-			local mixLine = model.getMix(item.channel-1, 0)
-			if mixLine.source == 84 then -- MAX
-				showSubmenu = true
-			end
+		local mixLine = model.getMix(item.channel-1, 0)
+		if mixLine.source == 84 then -- MAX
+			showSubmenu = true
 		end
+		-- If we should show submenu
 		if showSubmenu 
 		then
 			local current = parseValue(item)
@@ -125,15 +150,8 @@ local function run(event)
 			if event == EVT_ENTER_BREAK then
 
 				if next ~= current then
-					-- Change output
-					local mixLine = model.getMix(item.channel-1, 0)
-					if next == 0 then mixLine.weight=0
-					elseif next == 1 then mixLine.weight=100
-					elseif next == 2 then mixLine.weight=-100
-					elseif next == 3 then mixLine.weight=70
-					end
-					model.insertMix(item.channel-1, 0, mixLine)
-					model.deleteMix(item.channel-1, 1)
+					addLI({channel=item.channel, case=next, timeout=0})
+
 				end
 			end
 		end
